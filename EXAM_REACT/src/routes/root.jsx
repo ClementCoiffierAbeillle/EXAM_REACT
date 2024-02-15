86e00893069a2adbe3256a4812c38cf6312c3fb1
Blumaie <ccoiffier33@gmail.com>
2024-02-15 14:05:14 +0100

    ExamPartie1&2

---
+++ EXAM_REACT/src/routes/root.jsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import App from "../App";
+import ErrorPage from "../pages/404";
+import Home from "../pages/home";
+import MentionLegales from "../pages/mentionsLegales";
+import DetailPokemon from "../pages/detailFilm";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/mentions-legales", element: <MentionLegales /> },
+      { path: "/detailFilm/:idFilm", element: <DetailFilm /> },
+    ],
+  },
+]);
+
+export default router;
